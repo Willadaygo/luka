b57d9eadfa2c46d46d5080b16240b3207b2c84b9
Will Russell <will@studiorotate.com>
2017-06-25 10:48:05 +0100

    minor db amend

--- app/craft/config/db.php
+++ app/craft/config/db.php
@@ -10,35 +10,28 @@
 return [
 
 	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
+	// 'server' => 'localhost',
 
-	// The name of the database to select.
-	'database' => 'luka',
+	// // The name of the database to select.
+	// 'database' => 'luka',
 
-	// The database username to connect with.
-	'user' => 'root',
+	// // The database username to connect with.
+	// 'user' => 'root',
 
-	// The database password to connect with.
-	'password' => '',
+	// // The database password to connect with.
+	// 'password' => '',
 
-	// The prefix to use when naming tables. This can be no more than 5 characters.
+	// // The prefix to use when naming tables. This can be no more than 5 characters.
+	// 'tablePrefix' => 'craft',
+	'server'      => getenv('MYSQL_HOST')     ?: 'localhost',
+	'database'    => getenv('MYSQL_DATABASE') ?: 'luka',
+	'user'        => getenv('MYSQL_USER')     ?: 'root',
+	'password'    => getenv('MYSQL_PASSWORD') ?: '',
 	'tablePrefix' => 'craft',
+
+	// to be compatible with MySQL 5.7, requires 2.6.2949 or greater
+	'initSQLs'     => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
 
 ];
 
-// '*' => [
-// 	'environmentVariables' => [
-// 		'siteUrl' => 'https://luka.dev/'
-// 	],
-// 	'enableCsrfProtection' => true,
-// 	'omitScriptNameInUrls' => true,
-// 	'cpTrigger' => 'admin',
-// 	'devMode' => true,
-// 	'usePathInfo' => true,
-// ],
-// '.com' => [
-// 	'environmentVariables' => [
-// 		'siteUrl' => 'http://luka.com/'
-// 	],
-// 	'devMode' => false,
-// ]
+	
